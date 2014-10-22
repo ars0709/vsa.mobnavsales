37f0904fbdb21ea211ee5516a2ae530a1d9cafcc
ars0709 <aris.ridwan@gmail.com>
2014-10-22 09:55:40 +0700

    Update 22 10 2014 9:55
    Update For Data Created

---
+++ vsa.mobnavsales.android/Screens/datareceiver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace vsa.mobnavsales.android 
+{
+    [BroadcastReceiver]
+    public class DataReceiver : BroadcastReceiver
+    {
+        public event Action<Context, Intent> actionRecv;
+
+        #region implemented abstract members of BroadcastReceiver
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (this.actionRecv != null)
+            {
+                this.actionRecv(context, intent);
+            }
+        }
+        #endregion
+    }
+}
+
+
+ 
--- vsa.mobnavsales.android/Screens/navsetup.cs
+++ vsa.mobnavsales.android/Screens/navsetup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +11,22 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Android.Widget;
 using Android.Graphics;
+using vsa.mobnavsales.Library;
+ 
+
 
 namespace vsa.mobnavsales.android
 {
     [Activity(Label = "Navision Credential Setup")]
     public class navsetup : Activity
     {
+
+        IFungsiDB db = null;
+        DataTable dt = null;
+        DataReceiver broadcastRecv;
+        int EditID = -1;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -34,7 +44,143 @@ namespace vsa.mobnavsales.android
 
          
             // set our layout to be the home screen
-            SetContentView(Resource.Layout.NavSetup);    
+            SetContentView(Resource.Layout.NavSetup);
+            Button xbtnsave = FindViewById<Button>(Resource.Id.btnsave);
+            
+            xbtnsave.Click += xbtnsave_click;
+              if (db == null)
+            {
+                db = Common.setupDatabase();
+            }
+
+
+            broadcastRecv = new DataReceiver();
+            broadcastRecv.actionRecv += OnDataReceived;
+            RegisterReceiver(broadcastRecv, new IntentFilter(Common.ACTION_NEW_DATA));
         }
+
+
+        void cleanobject()
+        {
+            EditText xtxtws = FindViewById<EditText>(Resource.Id.txtwebservice);
+            EditText xtxtwserver = FindViewById<EditText>(Resource.Id.txtwebserver);
+            EditText xtxtwport = FindViewById<EditText>(Resource.Id.txtport);
+            xtxtws.Text = string.Empty;
+            xtxtwserver.Text = string.Empty;
+            xtxtwport.Text = string.Empty;
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            RegisterReceiver(broadcastRecv, new IntentFilter(Common.ACTION_NEW_DATA));
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            UnregisterReceiver(broadcastRecv);
+        }
+
+        void OnDataReceived(Context arg1, Intent arg2)
+        {
+            if (arg2 != null)
+            {
+                EditID = arg2.GetIntExtra("EditID", -1);
+                if (EditID <= 0)
+                    cleanobject();
+                else
+                    LoadDetail();
+
+            }
+        }
+
+        void RefreshData(bool State)
+        {
+            var NewData = new Intent(Common.ACTION_REFRESH_DATA);
+            NewData.PutExtra("Refresh", State);
+            SendBroadcast(NewData);
+        }
+
+
+        private void xbtnsave_click(object sender, System.EventArgs e)
+        {
+ 	         bool v_result = false;
+
+            EditText xtxtws = FindViewById<EditText>(Resource.Id.txtwebservice);
+            EditText xtxtwserver = FindViewById<EditText>(Resource.Id.txtwebserver);
+            EditText xtxtwport = FindViewById<EditText>(Resource.Id.txtport);
+
+            if (EditID < 0)
+            {
+                //new data
+                string[] FieldName = { "NavWebServiceName", "NavWebServer", "NavPort" };
+                object[] FieldData = { xtxtws.Text, xtxtwserver.Text, xtxtwport.Text };
+                v_result = db.InsertData("NavNetworkCredential", FieldName, FieldData);
+
+            }
+            else
+            {
+                //update
+                string[] FieldNameID = { "_ID" };
+                object[] FieldDataID = { EditID };
+                string[] FieldName = { "NavWebServiceName", "NavWebServer", "NavPort" };
+                object[] FieldData = { xtxtws.Text, xtxtwserver.Text, xtxtwport.Text };
+                v_result = db.UpdateRecord("NavNetworkCredential", FieldNameID, FieldDataID, FieldName, FieldData);
+            }
+
+            string v_message = string.Empty;
+            if (v_result)
+            {
+                v_message = "Nav Network Credential Created.";
+                Activity tabs = (Activity)this.Parent;
+               
+                RefreshData(true);
+            }
+            else
+            {
+                v_message = "Nav Network Credential Failed";
+            }
+            Toast a = Toast.MakeText(this.BaseContext, v_message, ToastLength.Short);
+            a.Show();
+        }
+
+
+
+        private void LoadDetail()
+        {
+            try
+            {
+                EditText xtxtws = FindViewById<EditText>(Resource.Id.txtwebservice);
+                EditText xtxtwserver = FindViewById<EditText>(Resource.Id.txtwebserver);
+                EditText xtxtwport = FindViewById<EditText>(Resource.Id.txtport);
+
+                DataTable dt = db.RetrieveData("select * from NavNetworkCredential where id=" + EditID);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        xtxtws.Text = dr["NavWebServiceName"].ToString();
+                        xtxtwserver.Text = dr["NavWebServer"].ToString();
+                        xtxtwport.Text = dr["NavPort"].ToString();
+                        
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Toast a = Toast.MakeText(this.BaseContext, ex.Message + ":" + ex.StackTrace, ToastLength.Short);
+                a.Show();
+            }
+        }
+
+     
+
+      
+       
+
+
     }
 }
